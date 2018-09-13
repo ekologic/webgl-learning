49c67ac1455258a500fa4c5c6bbfd625876a98f3
Bek Zod <ekologic90@gmail.com>
2018-09-13 22:33:02 +0200

    Initial commit;

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
